5d8809814e29f253a47663ff3dea69af94b85ef2
wenhao wang <felixwhwang@gmail.com>
2020-01-11 03:54:56 +0000

    problem added

---
+++ Edabit/concat.js
@@ -0,0 +1,25 @@
+// Concatenate Variable Number of Input Arrays
+// Create a function that concatenates n input arrays, where n is variable.
+
+//   Examples
+// concat([1, 2, 3], [4, 5], [6, 7]) ➞[1, 2, 3, 4, 5, 6, 7]
+
+// concat([1], [2], [3], [4], [5], [6], [7]) ➞[1, 2, 3, 4, 5, 6, 7]
+
+// concat([1, 2], [3, 4]) ➞[1, 2, 3, 4]
+
+// concat([4, 4, 4, 4, 4]) ➞[4, 4, 4, 4, 4]
+// Notes
+// Arrays should be concatenated in order of the arguments.
+
+const concat = (...args) => {
+  let result = [];
+  for(let index = 0; index < args.length; index++) {
+    for(let j = 0; j < args[index].length; j++) {
+      result.push(args[index][j]);
+    }
+  }
+  return result;
+}
+
+console.log(concat([1, 2, 3], [4, 5], [6, 7]));
