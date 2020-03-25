adeca3a83c82ac1e2f9160360ac1fbecb46e0d43
wenhao wang <felixwhwang@gmail.com>
2020-03-25 04:29:55 +0000

    more problems added

---
+++ Other-Practice/bubble-sort.js
@@ -0,0 +1,14 @@
+const bublesort = array => {
+  for(let i = 0; i < array.length - 1; i++) {
+    for(let j = 0; j < array.length - i - 1; j++) {
+      if(array[j] > array[j+1]) {
+        [array[j], array[j+1]] = [array[j+1], array[j]];
+      }
+    }
+  }
+  return array;
+}
+
+
+let array = [29,3,43,6,5,4,19,1,32,12,43,55,12,99,32,12];
+console.log(bublesort(array));
---
+++ Other-Practice/throttle-fn.js
@@ -0,0 +1,32 @@
+const throttle = (fn, ms) => {
+  let inThrottle;
+  return () => {
+    if(!inThrottle) {
+      fn();
+      inThrottle = true;
+      setTimeout(() => inThrottle = false, ms);
+    }
+  };
+}
+
+const fn = () => {
+  console.log('Hello');
+}
+
+const f = throttle(fn, 3000);
+
+f();
+f();
+f();
+f();
+f();
+f();
+//3s later
+setTimeout(() => {
+  f();
+  f();
+  f();
+  f();
+  f();
+  f();
+}, 3000);
