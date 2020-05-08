5eaab51e31e6edd49b6af02319b28a3bf0338d01
wenhao wang <felixwhwang@gmail.com>
2020-05-08 22:35:38 +0000

    more problem finished

---
+++ Edabit/count-character.js
@@ -0,0 +1,19 @@
+// Count Instances of a Character in a String
+// Create a function that takes two strings as arguments and returns the number of times the first string(the single character) is found in the second string.
+
+//   Examples
+// charCount("a", "edabit") ➞ 1
+
+// charCount("c", "Chamber of secrets") ➞ 1
+
+// charCount("b", "big fat bubble") ➞ 4
+// Notes
+// Your output must be case -sensitive(see second example).
+
+const charCount = (key, str) => {
+  return str.split('').reduce((sum, cur) => {
+    if(key === cur) return ++sum;
+    return sum;
+  }, 0);
+}
+console.log(charCount("b", "big fat bubble"));
