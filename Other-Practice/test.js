dab975b5538d84dcb5aa333d80cca5b6b5341434
wenhao wang <felixwhwang@gmail.com>
2020-08-24 05:37:22 +0000

    more problems added

---
+++ Other-Practice/test.js
@@ -0,0 +1,12 @@
+const test = num => {
+  let sum = 0;
+  let i = 1;
+  while(i <= num) {
+    sum += 2 * i;
+    i++;
+  }
+  let result = [sum, 11467 * num];
+  return result;
+}
+
+console.log(test(50));
