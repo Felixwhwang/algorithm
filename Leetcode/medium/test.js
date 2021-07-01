53889b4663e2e7778e37e91ee41ffeffa5df981f
wenhao wang <felixwhwang@gmail.com>
2021-07-01 23:37:28 +0000

    moving forward

--- Leetcode/medium/test.js
+++ Leetcode/medium/test.js
@@ -1,6 +1,14 @@
-let a = [1,2,3];
-let b = a.slice();
-a.pop();
+const insertion = arr => {
+  for(let i = 1; i < arr.length; i++) {
+    let key = arr[i];
+    let j = i - 1;
+    while(j >= 0 && arr[j] > key) {
+      arr[j+1] = arr[j--];
+    }
+    arr[j+1] = key;
+  }
+  return arr;
+}
 
 
-console.log(b);
+console.log(insertion([2,3,1,2,9,3,4,12,1]));
