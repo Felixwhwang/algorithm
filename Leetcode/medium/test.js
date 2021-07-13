fe8013c9dd94a92a4d2aad6af978bedfa8bf5778
wenhao wang <felixwhwang@gmail.com>
2021-07-13 06:53:24 +0000

    moving forward

--- Leetcode/medium/test.js
+++ Leetcode/medium/test.js
@@ -1,22 +1,12 @@
-const merge = (leftArr, rightArr) => {
-  let arr = [];
-  while (leftArr.length && rightArr.length) {
-    if (leftArr[0] < rightArr[0]) {
-      arr.push(leftArr.shift());
-    } else {
-      arr.push(rightArr.shift());
+const klarge = (arr, k) => {
+  for(let i = 0; i < k; i++) {
+    for(let j = 0; j < arr.length - 1 - i; j++){
+      if(arr[j] > arr[j+1]) {
+        [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+      }
     }
   }
-  return [...arr, ...leftArr, ...rightArr];
+  return arr[arr.length - k];
 }
 
-const MergeSort = arr => {
-  let mid = Math.floor(arr.length / 2);
-  if (arr.length < 2) {
-    return arr;
-  }
-  let left = arr.splice(0, mid);
-  return merge(MergeSort(left), MergeSort(arr));
-}
-
-console.log(MergeSort([2, 1, 3, 6, 12, 12, 0, 39, 9]));
+console.log(klarge([7, 10, 4, 3, 20, 15], 1));
